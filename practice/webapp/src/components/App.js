b1c6fb827ff04b7dc71008f72791afb4b677f5e6
tanishakumawat <113818130+tanishakumawat@users.noreply.github.com>
2024-11-07 16:59:07 +0530

    Add files via upload

---
+++ practice/webapp/src/components/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Item from './components/Item';
+import Dashbord from './components/dashboard';
+
+function App(){
+  // return <Item/>
+  return(
+    <Dashbord/>
+  )
+}
+
+
+export default App;
